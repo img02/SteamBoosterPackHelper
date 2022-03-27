e5ca38a0fefe698e9aad32ad644a47ec43da076a
William <will_sun@y7mail.com>
2022-03-27 23:17:23 +1100

    added function to get items ids from inventory.

--- SteamBoosterHelper/OpenBoosters.js
+++ SteamBoosterHelper/OpenBoosters.js
@@ -5,10 +5,30 @@ button.innerText = 'Open All Boosters';
 button.name = 'OpenBoosterButton';
 button.style.cssText += "border: 1px solid black; width: 125px; height: 40px;margin-right: 54px;";
 button.style.cssText += "color: black; border-radius: 4px;vertical-align: middle;float: right;";
+button.classList.add("btn_darkblue_white_innerfade", "btn_medium");
 
 let parent = document.getElementById('inventory_logos');
 //parent.style.border = "5px solid red";
 
+//button Event Listener
+button.addEventListener("click", ()=>{
+    getInventory();
+});
+
 parent.appendChild(button);
 
 
+
+// ◉_◉
+//functions
+var names = '';
+
+function getInventory(){
+var elements = document.getElementsByClassName("item app753 context6");
+for(var i = 0; i < elements.length; i++) {
+    names += elements[i].id + " ";
+    }
+    names = names.replaceAll("753_6_","");
+    console.log(names);
+    alert(names);
+}
