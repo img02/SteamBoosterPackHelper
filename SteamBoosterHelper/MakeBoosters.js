aef353c5059a61c0d64034e24f122d6636af2ae2
William <will_sun@y7mail.com>
2022-03-28 18:09:45 +1100

    fixed minor issue, added function for making booster preset appids

--- SteamBoosterHelper/MakeBoosters.js
+++ SteamBoosterHelper/MakeBoosters.js
@@ -1,28 +1,42 @@
-{
+//document.body.style.border = "5px solid red";
 
-  "manifest_version": 2,
-  "name": "SteamBoosterHelper",
-  "version": "1.0",
+let button = document.createElement('button');
+button.innerText = 'Make My Boosters';
+button.name = 'MakeBoostersButton';
+button.style.cssText += "border: 1px solid black; width: 125px; height: 40px;margin-right: 54px;";
+button.style.cssText += "color: black; border-radius: 4px; margin-left:50px;";
 
-  "description": "Quickly open and create all your booster backs.",
+//button.classList.add("btn_darkblue_white_innerfade", "btn_medium");
+button.classList.add("btnv6_blue_blue_innerfade", "btn_medium", "btn_makepack");
 
-  "icons": {
-    "48": "icons/icon.png"
-  },
+let parent = document.querySelector(".booster_game_selector");
+parent.style.width = "600px";
 
-  "content_scripts": [
-    {
-      "matches": ["*://*.steamcommunity.com/id/*/inventory/"],
-      "js": ["OpenBoosters.js"]
-    },
-	{
-      "matches": [
-	  "*://*.steamcommunity.com/tradingcards/boostercreator", 
-	  "*://*.steamcommunity.com/tradingcards/boostercreator/*"
-	  ],
-	  
-      "js": ["MakeBoosters.js"]
-    }
-  ]
+//button Event Listener
+button.addEventListener("click", ()=>{
+    //getBoosterAppIds();
+    createScriptString();
+    MakeBoosters();
+});
 
+parent.appendChild(button);
+//parent.style.border = "5px solid red";
+
+//functions
+
+let scriptString = '';
+
+function MakeBoosters(){ //inject script
+	let script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.textContent = scriptString;
+	document.head.appendChild(script);
+}
+
+function createScriptString(){
+	let appids = ['1328670', '1343370', '524220', '1113560', '1158850', '787480', '412830', '324160', '1113000', '447530' ];
+	
+	appids.forEach( (id)=> 
+	scriptString += `CBoosterCreatorPage.ExecuteCreateBooster({appid: '${id}',series:'1'}, '2'); `);
+	alert(scriptString);
 }
