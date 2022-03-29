b739754a7a7c48b8094a5af9b4bb91fa277eca2e
William <will_sun@y7mail.com>
2022-03-29 19:22:38 +1100

    minor change to improve user experience when opening boosters

--- SteamBoosterHelper/chrome/OpenBoosters.js
+++ SteamBoosterHelper/chrome/OpenBoosters.js
@@ -30,8 +30,8 @@ InjectOpenBoosters();
 
 // ◉_◉
 //functions
-var names = '';
-var scriptString = '';
+let names = '';
+let scriptString = '';
 
 function InjectOpenBoosters() { //inject script    
     let script = document.createElement('script');
@@ -43,10 +43,15 @@ function InjectOpenBoosters() { //inject script
 }
 
 function getInventory() {
+	names = ''; //reset string
     //gets the first 50 items in inventory, unless user loads more pages
-    var elements = document.getElementsByClassName("item app753 context6");
+    let elements = document.getElementsByClassName("itemHolder");	
     for (var i = 0; i < elements.length; i++) {
-        names += elements[i].id + " ";
+		if (elements[i].style.getPropertyValue('display') != "none"){
+			if (elements[i].firstChild != null){
+        names += elements[i].firstChild.id + " ";
+			}
+		}
     }
     names = names.replaceAll("753_6_", "");
     console.log(names);
@@ -54,11 +59,14 @@ function getInventory() {
 }
 
 function createScriptString() {
+	scriptString = ''; //reset string
     let idArr = names.split(" ");
     for (var i = 0; i < idArr.length; i++) {
         //123 is a throwaway number, normally this should represent the appid 
         //and is used in the receiving element for linking the relevant games badge page.
+        if (idArr[i] != 'undefined' && idArr[i] != ''){
         scriptString += "OpenBooster('123','" + idArr[i] + "');";
+		}
     }
     //alert(scriptString);
     console.log(scriptString);
--- SteamBoosterHelper/firefox/OpenBoosters.js
+++ SteamBoosterHelper/firefox/OpenBoosters.js
@@ -26,8 +26,8 @@ parent.appendChild(button);
 
 // ◉_◉
 //functions
-var names = '';
-var scriptString = '';
+let names = '';
+let scriptString = '';
 
 function OpenBoosters() { //inject script
     let script = document.createElement('script');
@@ -37,10 +37,16 @@ function OpenBoosters() { //inject script
 }
 
 function getInventory() {
+	names = ''; //reset string
     //gets the first 50 items in inventory, unless user loads more pages
-    let elements = document.getElementsByClassName("item app753 context6");
+    //let elements = document.getElementsByClassName("item app753 context6");
+    let elements = document.getElementsByClassName("itemHolder");	
     for (var i = 0; i < elements.length; i++) {
-        names += elements[i].id + " ";
+		if (elements[i].style.getPropertyValue('display') != "none"){
+        if (elements[i].firstChild != null){
+        names += elements[i].firstChild.id + " ";
+			}
+		}
     }
     names = names.replaceAll("753_6_", "");
     console.log(names);
@@ -48,11 +54,14 @@ function getInventory() {
 }
 
 function createScriptString() {
+	scriptString = ''; //reset string
     let idArr = names.split(" ");
     for (var i = 0; i < idArr.length; i++) {
         //123 is a throwaway number, normally this should represent the appid 
         //and is used in the receiving element for linking the relevant games badge page.
+		if (idArr[i] != 'undefined' && idArr[i] != ''){
         scriptString += "OpenBooster('123','" + idArr[i] + "');";
+		}
     }
     //alert(scriptString);
     console.log(scriptString);
